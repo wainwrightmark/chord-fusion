a113cebde7e45a81dbee43752d45771171ad6c4b
Mark Wainwright <wainwrightml@gmail.com>
2022-08-24 23:01:58 +0100

    Changed to different ci

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,11 @@ mod deconstructor;
 use deconstructor::*;
 
 fn main() {
+
+    // When building for WASM, print panics to the browser console
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
+
     App::new()
         .insert_resource(LogSettings {
             level: Level::INFO,
