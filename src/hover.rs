1096c99cbf3070b393f5848d002c90826bd9115f
Mark Wainwright <wainwrightml@gmail.com>
2022-08-24 08:42:27 +0100

    Fixed bad sound whilst dragging

--- src/hover.rs
+++ src/hover.rs
@@ -1,7 +1,9 @@
-use bevy::{prelude::*, utils::HashSet};
+use bevy::{
+    prelude::*,
+    utils::{HashMap, HashSet},
+};
 
 use crate::*;
-
 
 pub struct HoverPlugin;
 impl Plugin for HoverPlugin {
@@ -19,10 +21,10 @@ pub fn detect_hover(
     q_camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
     rapier_context: Res<RapierContext>,
     orbs: Query<(Entity, Option<&PlayingSound>), With<Orb>>,
-    current_sounds: Query<Entity, With<PlayingSound>>,
+    current_sounds: Query<(Entity, Option<&Dragged>), With<PlayingSound>>,
 ) {
     if let Some(_ev) = cursor_evr.iter().last() {
-        let mut c_sounds: HashSet<Entity> = current_sounds.into_iter().collect();
+        let mut c_sounds: HashMap<Entity, Option<&Dragged>> = current_sounds.into_iter().collect();
 
         if let Some(position) = get_cursor_position(windows, q_camera) {
             rapier_context.intersections_with_point(position, default(), |entity| {
@@ -38,8 +40,10 @@ pub fn detect_hover(
             });
         }
 
-        for e in c_sounds {
-            commands.entity(e).remove::<PlayingSound>();
+        for (e, dragged) in c_sounds {
+            if dragged.is_none() {
+                commands.entity(e).remove::<PlayingSound>(); //Don't stop the sound if the entity is being dragged
+            }
         }
     }
 }
